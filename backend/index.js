85904dac69622d07082e512c1ec17f9077655e86
Rudresh-sisodiya <rudresh.sisode@creolestudios.com>
2026-04-25 17:43:56 +0530

    feat: call initDB on server startup to create tables

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+const { initDB } = require('./db')
 
 const app = express()
 const PORT = 4000
 
 app.use(cors({ origin: 'http://localhost:5173' }))
 app.use(express.json())
+
+initDB()
 
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`)
